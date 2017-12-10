61674f50e4dd4dff97957e34bd07f44e49ec05df
Andrey Liapin <liapin.andrey@gmail.com>
2017-12-10 21:00:01 +0200

    implemented task 3

--- lab11/main.js
+++ lab11/main.js
@@ -29,8 +29,37 @@ function getFragment(copyCounter) {
 const elem = document.getElementsByClassName('play')[0]
 document.getElementById('playAnimation').onclick = (e) => {
   e.preventDefault();
-  if(parseInt(elem.style.left) === 0)
+  if(!elem.style.left || parseInt(elem.style.left) === 0)
     elem.style.left = elem.parentElement.offsetWidth - elem.offsetWidth + 'px';
   else
     elem.style.left = 0 + 'px';
+}
+
+//Task 3
+function showAlert(type) {
+  let container = document.getElementById('alert-container');
+  let text = document.getElementById('alert-text').value
+  container.appendChild(generateAlert(type, text));
+}
+
+function generateAlert(type, text) {
+  
+  let element = document.createElement('div');
+  let alertTimer = setTimeout(() => { removeAlert(element) }, 1000 * 10)
+  element.setAttribute('class', 'alert alert-' + type)
+  element.addEventListener('click', (e) => {
+    e.preventDefault();
+    clearTimeout(alertTimer);
+    removeAlert(element);
+  });
+  
+  element.innerHTML += '<a href="#" class="close" title="close">&times;</a>'
+  element.innerHTML += "<strong class='text-capitalize'>" + type + '!</strong> '
+  element.innerHTML += text
+  return element;
+}
+
+function removeAlert(element) {
+  element.className += " closed";
+  setTimeout(() => { element.remove(); }, 700)
 }
