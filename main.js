76d586e760889bb6d0b02ccf7ede2298caaccc09
Andrey Liapin <liapin.andrey@gmail.com>
2017-12-10 16:08:37 +0200

    implemented task 1

---
+++ main.js
@@ -0,0 +1,26 @@
+//Task 1
+let btn = document.getElementById('insertButton');
+let copyCounter = 0;
+btn.onclick = () => {
+  const position = document.getElementsByTagName('select')[0].selectedIndex;
+  const container = document.getElementById('container');
+  switch(position) {
+    case 0:
+      container.insertAdjacentElement('afterBegin', getFragment(++copyCounter));
+      break;
+    case 1:
+      container.insertBefore(getFragment(++copyCounter), container.children[Math.floor(copyCounter/2)])
+      break;
+    case 2:
+      container.appendChild(getFragment(++copyCounter));
+      break;
+  }
+};
+
+function getFragment(copyCounter) {
+  let fragment = document.getElementsByTagName('pre')[0].innerHTML;
+  fragment = fragment.replace('#', copyCounter)
+  const pre = document.createElement('pre')
+  pre.innerHTML = fragment
+  return pre
+}
